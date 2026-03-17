b92ca5570574cc20b8946ff4a1203364893133a9
DanteSM16 <hsaini200025@gmail.com>
2026-03-17 13:55:13 +0530

    Update SalesInvoiceRequest.java

--- src/main/java/com/inventory/gst_billing/dto/SalesInvoiceRequest.java
+++ src/main/java/com/inventory/gst_billing/dto/SalesInvoiceRequest.java
@@ -11,5 +11,11 @@ public class SalesInvoiceRequest {
     private String stateCode; // Determines IGST vs CGST/SGST
     private String customerGstin; //send if b2b mandatory for b2b b2c segregation,
 
+    //  PAYMENT DATA
+    private String paymentMethod; // "RAZORPAY_ONLINE" or "CASH"
+    private String razorpayPaymentId;
+    private String razorpayOrderId;
+    private String razorpaySignature; // The cryptographic proof!
+
     private List<SalesItemRequest> items; // Scanned items
 }
